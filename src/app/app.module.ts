2249890b92c8a063e51ba9a280bf2df000484308
csepulvedaa <crsepulv@dcc.uchile.cl>
2021-11-15 02:23:01 -0300

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+    import { NgModule } from '@angular/core';
+    import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+   //Servicios
+    import { HeroesService } from './services/heroes.service';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+
+    //rutas
+    import { AppRoutingModule } from './app-routing.module';
+    //componentes
+    import { AppComponent } from './app.component';
+    import { NavbarComponent } from './components/shared/navbar/navbar.component';
+    import { HomeComponent } from './components/home/home.component';
+    import { AboutComponent } from './components/about/about.component';
+    import { HeroesComponent } from './components/heroes/heroes.component';
+
+    @NgModule({
+      declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AboutComponent,
+        HeroesComponent
+      ],
+      imports: [
+        BrowserModule,
+        AppRoutingModule
+      ],
+      providers: [
+        HeroesService
+      ],
+      bootstrap: [AppComponent]
+    })
+    export class AppModule { }
---
+++ src/app/services/heroes.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HeroesService {
+    constructor(){
+        console.log("Servicio listo para usar!")
+    }
+}
